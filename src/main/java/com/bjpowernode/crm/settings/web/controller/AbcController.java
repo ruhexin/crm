c37bd1030a759c4248b514b5d7c122cb74edc9bb
ruhexin <a15691415373@163.com>
2022-10-30 14:53:11 +0800

    hot-fix新建类 修改

--- src/main/java/com/bjpowernode/crm/settings/web/controller/AbcController.java
+++ src/main/java/com/bjpowernode/crm/settings/web/controller/AbcController.java
@@ -1,9 +1,11 @@
 package com.bjpowernode.crm.settings.web.controller;
 
+import java.sql.SQLOutput;
+
 public class AbcController {
 
 
-
-
-
+    public static void main(String[] args) {
+        System.out.println("hot-fix");
+    }
 }
